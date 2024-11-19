92a542571d47ae2974af4093fb312973c83e94de
aderemi <73750355+gabrieladeremi@users.noreply.github.com>
2024-11-19 17:02:43 +0000

    add tool implementation

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,4 +6,34 @@ class ToolsController < ApplicationController
   def show
     @tool = Tool.find(params[:id])
   end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    @tool.user = current_user
+
+    if @tool.save!
+      redirect_to tool_path(@tool)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(
+      :name,
+      :description,
+      :price,
+      :address,
+      :category,
+      :available_from,
+      :available_until,
+      images: []
+    )
+  end
 end
